a3a3b4c4e893de79ecabd5fa66772e17671a7172
Daniel Lamando <apt@danopia.net>
2016-09-12 23:26:50 -0700

    Relocate lambda deploy code

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,5 @@ aws s3 sync web/ s3://uber.danopia.net/
 
 cd lambdas
 ./build.sh
+aws lambda update-function-code --function-name UberNet-Register --zip-file fileb://lambdas/.compiled/register.zip > /dev/null
+aws lambda update-function-code --function-name UberNet-Beacon --zip-file fileb://lambdas/.compiled/beacon.zip > /dev/null
--- lambdas/build.sh
+++ lambdas/build.sh
@@ -10,7 +10,3 @@ cd ..
 cd beacon
 zip -ruq ../.compiled/beacon.zip *
 cd ..
-
-# aws s3 sync .compiled s3://ubernet/compiled-lambdas
-aws lambda update-function-code --function-name UberNet-Register --zip-file fileb://.compiled/register.zip > /dev/null
-aws lambda update-function-code --function-name UberNet-Beacon --zip-file fileb://.compiled/beacon.zip > /dev/null
